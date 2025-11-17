2a63b4afe8ca55733bd626375bd879344328acd0
Thanh Tung Nguyen <thickhoctin@gmail.com>
2025-11-17 10:45:53 +0100

    Update experiment directory for fourway intersection evaluation

--- launch_train.sh
+++ launch_train.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Example evaluation hyperparameters
-EXP_DIR=results/fourway_2x1_penetration0.5
+EXP_DIR=results/fourway_1x1_penetration0.5_turn
 
 python3 intersection.py $EXP_DIR 
-    #render
