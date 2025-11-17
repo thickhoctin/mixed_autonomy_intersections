6e1c35f2a11349a8d4731f70dbb12a312130629b
Thanh Tung Nguyen <thickhoctin@gmail.com>
2025-11-17 10:45:48 +0100

    Update evaluation script parameters for fourway intersection scenario

--- launch_eval_fourway_1x1_pene0.5.sh
+++ launch_eval_fourway_1x1_pene0.5.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Example evaluation hyperparameters
-EXP_DIR=results/fourway_2x1_penetration0.5
-CKPT=0
-FR_H=850 # Horizontal flow rate in vehicles/hour
-FR_V=850 # Vertical flow rate
-N_ROWS=2
+EXP_DIR=results/fourway_1x1_penetration0.5_turn
+CKPT=100
+FR_H=700 # Horizontal flow rate in vehicles/hour
+FR_V=700 # Vertical flow rate
+N_ROWS=1
 N_COLS=1
-RESULT_SAVE_PATH=$EXP_DIR/eval_results/e0_2x1_skip500_flow850x850.csv
+RESULT_SAVE_PATH=$EXP_DIR/eval_results/e100_1x1_skip500_flow700x700.csv
 DIRECTION="4way"
 
 python3 intersection.py $EXP_DIR \
