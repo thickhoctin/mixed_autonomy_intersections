d65aebcc3660cd6c2ec323952b62605e9afa8f20
Thanh Tung Nguyen <thickhoctin@gmail.com>
2026-02-11 00:21:30 +0100

    Refactor code structure for improved readability and maintainability

--- copy_net.py
+++ copy_net.py
@@ -1,5 +1,5 @@
-ckpt_path = 'results/fourway_1x1_penetration1_turn_adam_ppo_12.12/models/flow_700x700/model-175.pth'
-new_ckpt_path = 'results/fourway_1x1_penetration0.5_turn_adam_ppo_14.12/models/flow_700x700/model-0.pth'
+ckpt_path = 'results/fourway_1x1_penetration0.5_test/models/flow_700x700/model-0.pth'
+new_ckpt_path = 'results/fourway_1x1_penetration0.5_test/models/flow_700x700/model-5.pth'
 
 import torch
 model_dict = torch.load(ckpt_path)
--- launch_train.sh
+++ launch_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Example evaluation hyperparameters
-EXP_DIR=results/fourway_1x1_penetration0.5_turn_adam_ppo/
+EXP_DIR=results/fourway_1x1_penetration0.5_turn_adam_ppo_transformer_09.02/
 
-python3 intersection.py $EXP_DIR 
+python3 intersection.py $EXP_DIR load_step=5
