0151edd42002b8cbbc52078a8fb3cd3798b65e1e
Thanh Tung Nguyen <thickhoctin@gmail.com>
2026-02-16 10:42:55 +0100

    add results and everything else

--- launch_train.sh
+++ launch_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Example evaluation hyperparameters
-EXP_DIR=results/fourway_1x1_penetration0.5_turn_adam_ppo_transformer_09.02/
+EXP_DIR=results/fourway_1x1_penetration0.5_turn_adam_ppo_transformer_13.02/
 
-python3 intersection.py $EXP_DIR load_step=5
+python3 intersection.py $EXP_DIR load_step=85
