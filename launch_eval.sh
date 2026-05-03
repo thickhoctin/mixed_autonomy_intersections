a6e6eaa05b5d8cd0f8bf52f4355d0bc3da96daae
Thanh Tung Nguyen <thickhoctin@gmail.com>
2026-05-03 17:23:43 +0200

    Refactor code structure for improved readability and maintainability

--- launch_eval.sh
+++ launch_eval.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Example evaluation hyperparameters
-EXP_DIR=results/twoway_2x1_penetration0.333
-CKPT=200
-FR_H=850 # Horizontal flow rate in vehicles/hour
+EXP_DIR=results/fourway_1x1_penetration0.333_turn_adam_ppo_15.12
+CKPT=260
+FR_H=700 # Horizontal flow rate in vehicles/hour
 FR_V=700 # Vertical flow rate
-N_ROWS=3
-N_COLS=3
-RESULT_SAVE_PATH=$EXP_DIR/eval_results/e165_3x3_skip500_flow850x700.csv
-
+N_ROWS=1
+N_COLS=1
+RESULT_SAVE_PATH=$EXP_DIR/eval_results/e260_1x1_skip500_flow700x700.csv
+VEHICLE_INFO_SAVE_PATH=$EXP_DIR/vehicle_info/e260_1x1_skip500_flow700x700_vehicle_info.csv
+mkdir -p "$EXP_DIR/eval_results" "$EXP_DIR/vehicle_info" \
+    && echo "Created directories for evaluation results and vehicle info in $EXP_DIR"
 python3 intersection.py $EXP_DIR \
     e=$CKPT \
     n_rows=$N_ROWS \
@@ -19,4 +21,7 @@ python3 intersection.py $EXP_DIR \
     flow_rate_h=$FR_H \
     flow_rate_v=$FR_V \
     result_save=$RESULT_SAVE_PATH \
-    render
+    vehicle_info_save=$VEHICLE_INFO_SAVE_PATH \
+    use_ray=False \
+    
+    
--- launch_train.sh
+++ launch_train.sh
@@ -3,4 +3,4 @@
 # Example evaluation hyperparameters
 EXP_DIR=results/fourway_1x1_penetration0.5_turn_adam_ppo_transformer_13.02/
 
-python3 intersection.py $EXP_DIR load_step=85
+python3 intersection.py $EXP_DIR load_step=975
